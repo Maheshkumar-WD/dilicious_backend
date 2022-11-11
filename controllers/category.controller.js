f01ac3ae8c306c8eebfcc514b287c46c0ccc60e5
maheshkumar-wd <maheshkumaryadav1430@gmail.com>
2022-11-11 19:36:47 +0530

    adding get route for category by id

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,6 +1,15 @@
 const { CategoryModel } = require("../models/category.model");
 
 let categoryController = {
+     getCatById:async (req,res)=>{
+          try{
+               let {cat_id}=req.params;
+               let categories = await CategoryModel.find({_id:cat_id});
+               res.send(categories);
+          }catch{
+               res.send("can't send data right now ");
+          }
+     },
      getCategory:async(req,res)=>{
           try{
                let categories = await CategoryModel.find({});
