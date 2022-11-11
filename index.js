dc85174ec66e5211d72d3a45ac5d7675626efc8b
maheshkumar-wd <maheshkumaryadav1430@gmail.com>
2022-11-11 23:19:47 +0530

    added subcategory routes api

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const address = require("address");
 const cors = require("cors");
 const { connection } = require("./configs/db");
 const { restaurentRoute } = require("./routes/restaurent.route");
 const { categoryRoute } = require("./routes/category.route");
 const { foodItemRoute } = require("./routes/footitem.route");
+const { subCategoryRoute } = require("./routes/subcategory.route");
 require("dotenv").config();
 // --------------
 const app = express();
@@ -18,6 +18,8 @@ app.get("/", (req, res) => {
 app.use("/restaurent", restaurentRoute);
 app.use("/category", categoryRoute);
 app.use("/fooditems", foodItemRoute);
+app.use("/subcat",subCategoryRoute);
+
 
 // ------------------------
 const PORT = process.env.PORT || 8080;
